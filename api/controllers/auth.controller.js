72f0e2854a25461e7f097f19e146051ffedb3da7
vermaji <rajaditya14032002@gmail.com>
2023-12-07 10:48:43 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
+import { User } from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const hashpassword = bcryptjs.hashSync(password, 10); //  hashsync use krna se await lagane ka koi jarurat nai hai kyunki ye synchronous function hai
+    const newUser = new User({ username, email, password: hashpassword });
+    const savedUser = await newUser.save();
+    res.status(201).json({
+      message: "user created successfully",
+      user: savedUser,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "username or email already exists", message: error });
+  }
+};
