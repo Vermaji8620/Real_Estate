6d210d90081400602c741b2481eddf9605bf62f3
vermaji <rajaditya14032002@gmail.com>
2023-12-08 08:29:59 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import bcryptjs from "bcryptjs";
 import { User } from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
@@ -12,6 +14,27 @@ export const signup = async (req, res, next) => {
       message: "user created successfully",
       user: savedUser,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "2h",
+    });
+    const copyValidUser = validUser.toObject();
+    delete copyValidUser.password;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(copyValidUser);
   } catch (error) {
     next(error);
   }
