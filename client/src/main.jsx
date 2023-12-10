40de07f1de0007124030b6829288b7859ce37e05
vermaji <rajaditya14032002@gmail.com>
2023-12-10 14:44:32 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // yaha pe batana hoga ki hm dukan ko set up krrhe hai...to Provider k andar me dukan ko wrap up krna hoga
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,28 @@
 // redux k bina v kaaam chal skta hai...but for large apps we use redux
 // yaha pe configuration set up krrhe hai...aur sb reducers ko ek sath 'user' k andar me integrate krrhe hai
 
-// in sb chiz ko redux-dev tools (edge browser )  pe dekh skte hai...but page refresh krne se information ud jata hai...to ek npm package use krnge 
+// in sb chiz ko redux-dev tools (edge browser )  pe dekh skte hai...but page refresh krne se information ud jata hai...to ek npm package use krnge (local storage me information store krne k liye)
+// redux-persist is a library for persisting and rehydrating a Redux store. It's commonly used in Redux applications to persist the state of the store to a storage medium, such as localStorage or AsyncStorage, allowing the state to be preserved across page reloads, app restarts, or even device reboots in the case of mobile applications,.
 
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({ user: userReducer });
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
